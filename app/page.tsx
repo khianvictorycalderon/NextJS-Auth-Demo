822d6c85739445a5af1caa41addd4cc2896933fe
khian calderon <ckhianvictory@gmail.com>
2025-08-15 22:26:15 +0800

    Working loading button

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,11 @@ export default function _() {
   }, []);
 
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+          <div className="w-12 h-12 border-4 border-gray-400 border-t-transparent rounded-full animate-spin" />
+      </div>
+    );
   }
 
   return (
